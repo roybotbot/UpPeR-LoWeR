24141522eebb0b7cd8d828199bdbc89bd22235ca
royimous <royimous@gmail.com>
2020-06-12 02:21:06 -0400

    added some comments

--- UpPeR-LoWeR_Pyto-iOS.py
+++ UpPeR-LoWeR_Pyto-iOS.py
@@ -1,18 +1,1 @@
-# UpPeR-LoWeR_iOS.py
-
-import sys
-
-sTrInG = sys.argv[1]
-
-NeW_StRiNg = ""
-iNdEx = 0
-for i in sTrInG:
-	if i != " " and iNdEx % 2 == 0:
-			NeW_StRiNg += i.upper()
-	else:
-		NeW_StRiNg += i
-	iNdEx += 1
-
-print(NeW_StRiNg)
-
-
+w
